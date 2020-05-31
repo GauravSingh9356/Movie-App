ad71aaa4314bdfb6ceb982f1d1fe30c62b1dc550
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-06-01 01:10:21 +0530

    version 2.0.0

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 $(document).ready(() => {
+    axios.get("https://www.omdbapi.com?s=avengers&apikey=5fe7c608")
+      .then((response) => {
+        console.log(response);
+        let movies = response.data.Search;
+        let output = '';
+        $.each(movies, (index, movie) => {
+          output += `
+            <div class="col-md-3">
+              <div class="well text-center">
+                <img src="${movie.Poster}">
+                <h5>${movie.Title}</h5>
+                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+              </div>
+            </div>
+          `;
+        });
+  
+        $('#movies').html(output);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+
+
     $('#searchForm').on('submit', (e) => {
       let searchText = $('#searchText').val();
       getMovies(searchText);
